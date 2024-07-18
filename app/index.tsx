c55d057c1bbdf52d9689801da784b9049ca8e2ab
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-07-18 15:35:48 +0800

    created time context

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+/**
+ *
+ * fb function
+ * server time:
+ * previously stored time:
+ * relative time:
+ * sync
+ *
+ */
