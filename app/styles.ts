0876e453e75ae607948e111c73e29e39ee4834f1
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-07-19 14:15:18 +0800

    styles for heading, label, input and button

--- app/styles.ts
+++ app/styles.ts
@@ -10,4 +10,29 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  heading: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  label: {
+    fontSize: 12,
+    fontWeight: "light",
+    textAlign: "left",
+    alignSelf: "center",
+    maxWidth: 200,
+  },
+  input: {
+    borderColor: "#aaa",
+    borderWidth: 0.8,
+    paddingHorizontal: 15,
+    paddingVertical: 4,
+    borderRadius: 4,
+    minWidth: 200,
+    marginBottom: 10,
+  },
+  button: {
+    marginVertical: 5,
+    paddingHorizontal: 10,
+  },
 });
