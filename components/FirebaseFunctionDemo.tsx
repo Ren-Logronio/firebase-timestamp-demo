8e6335276d71caee0eceb306b69b4feb2ca0f025
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-07-19 14:16:32 +0800

    layout gaps

--- components/FirebaseFunctionDemo.tsx
+++ components/FirebaseFunctionDemo.tsx
@@ -1,9 +1,9 @@
 import { Suspense, useEffect, useState } from "react";
 import SuspenseActivityIndicator from "./SuspenseActivityIndicator";
 import { Button, Text } from "react-native";
-import { getTimestamp } from "@/utils/firebase";
-import { HttpsCallableResult } from "firebase/functions";
+import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import VerticalGap from "./VerticalGap";
 
 type ServerTimestampType = {
   client: {
@@ -19,43 +19,50 @@ type ServerTimestampType = {
 };
 
 export default function FirebaseFunctionDemo() {
-  const [loading, setLoading] = useState({ storage: true, server: true });
   const [serverTimestamp, setServerTimestamp] =
     useState<ServerTimestampType | null>(null);
-  const [storedTimestamp, setStoredTimestamp] = useState<string | null>(null);
-
-  const loadStoredTimestamp = async () => {
-    loading.storage && setLoading({ ...loading, storage: true });
-    const timestamp = await AsyncStorage.getItem("storedTimestamp");
-    setStoredTimestamp(timestamp || null);
-    setLoading({ ...loading, storage: false });
-  };
 
   const syncServerTimestamp = async () => {
-    loading.server && setLoading({ ...loading, server: true });
-    // getTimestamp({ query: { timestamp: storedTimestamp } }).then(
-    //   (response: HttpsCallableResult) => {
-    //     console.log("🚀 ~ syncServerTimestamp ~ response:", response);
-    //     setLoading({ ...loading, server: false });
-    //   }
-    // );
+    const storedTimestamp = await AsyncStorage.getItem("storedTimestampFn");
+    const response = await axios.get(
+      `https://timestamp-6ntz5wqtla-uc.a.run.app/timestamp${
+        storedTimestamp ? `?timestamp=${storedTimestamp}` : ""
+      }`
+    );
+    const { server, client } = response.data.timestamp;
+    const { difference, differenceRelativeTime } = response.data;
+    setServerTimestamp({
+      server,
+      client,
+      difference,
+      differenceRelativeTime,
+    });
+    AsyncStorage.setItem("storedTimestampFn", server?.iso);
   };
 
   useEffect(() => {
-    loadStoredTimestamp();
     syncServerTimestamp();
   }, []);
 
   return (
-    <Suspense fallback={<SuspenseActivityIndicator />}>
+    <>
       {serverTimestamp && serverTimestamp?.server && (
         <>
-          <Text>server time: {serverTimestamp?.server?.iso}</Text>
-          <Text>previously stored timestamp: {storedTimestamp || "N/A"}</Text>
-          <Text>duration: {serverTimestamp?.differenceRelativeTime}</Text>
+          <Text>server timestamp: </Text>
+          <Text>{serverTimestamp?.server?.iso}</Text>
+          <VerticalGap height={5} />
+          <Text>previously stored timestamp:</Text>
+          <Text>{serverTimestamp?.client?.iso || "N/A"}</Text>
+          <VerticalGap height={5} />
+          <Text>relative time: {serverTimestamp?.differenceRelativeTime}</Text>
+          <VerticalGap height={10} />
         </>
       )}
-      <Button title="sync timestamps" onPress={syncServerTimestamp} />
-    </Suspense>
+      <Button
+        disabled={!serverTimestamp || !serverTimestamp?.server}
+        title="sync timestamps"
+        onPress={syncServerTimestamp}
+      />
+    </>
   );
 }
