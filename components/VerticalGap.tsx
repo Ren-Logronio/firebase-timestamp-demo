0e0f899f1c147a510fb904f32ef34a400d3d7e17
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-07-19 14:16:53 +0800

    Create VerticalGap.tsx

---
+++ components/VerticalGap.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+const VerticalGap = ({ height }: { height: number }) => (
+  <View
+    style={{
+      height,
+    }}
+  ></View>
+);
+
+export default VerticalGap;
